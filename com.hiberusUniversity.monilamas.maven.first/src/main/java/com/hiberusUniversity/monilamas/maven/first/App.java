0f4618581291fd724a8a31956af7f4767a4f5b2c
moniLamas <mlamasrial@gmail.com>
2022-04-22 10:09:25 +0200

    first commit

---
+++ com.hiberusUniversity.monilamas.maven.first/src/main/java/com/hiberusUniversity/monilamas/maven/first/App.java
@@ -0,0 +1,14 @@
+package com.hiberusUniversity.monilamas.maven.first.App;
+
+/**
+ * Hello world!
+ * Good morning!
+ *
+ */
+public class cApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
